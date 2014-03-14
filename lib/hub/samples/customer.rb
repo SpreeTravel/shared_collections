a27f008707d296d8da84eb16e32e860e9b676844
Brian Quinn <briandquinn@gmail.com>
2014-03-14 13:52:10 +0000

    Add customer sample

---
+++ lib/hub/samples/customer.rb
@@ -0,0 +1,26 @@
+module Hub::Samples
+  class Customer < Base
+    class << self
+      def object
+        parsed_json('customer')
+      end
+
+      def request
+        {
+          'request_id' => request_id,
+          'shipment' => object['customer']
+        }
+      end
+
+      def message
+        {
+          'message' => 'customer:created',
+          'store_id' => "51f788cf65762d3177000001",
+          'payload' => {
+            'customer' => object['customer']
+          }
+        }
+      end
+    end
+  end
+end
